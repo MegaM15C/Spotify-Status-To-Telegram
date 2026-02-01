8685511437ae5f9f03ff642dd347e309b625a270
DNGRMXM <Belyhm659@gmail.com>
2026-02-01 12:15:42 +0300

    add: модульность

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
+import asyncio
 import time
 import os
-import typing
 import spotipy
-import asyncio
-
-import modules.datet as timestp
-
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyOAuth
-from telethon.sync import TelegramClient
-from telethon import functions, types
-
+from telethon import TelegramClient, functions
+import modules.datet as timestp
+from modules.auth import get_session
 
 load_dotenv()
 
+# Init Spotify client API
 spotify = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
+    auth_manager=spotipy.SpotifyOAuth(
         scope="user-read-currently-playing",
         client_id=os.getenv('SPOTIFY_CLIENT_ID'),
         client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
@@ -23,58 +19,49 @@ spotify = spotipy.Spotify(
         username=os.getenv('SPOTIFY_USERNAME'),
     )
 )
+
 inst = ' | inst: @dngrmax'
-user = '@' + 'dngrmax'
-current_playing = typing.List[typing.Union[str, str, str]]
+user = '@dngrmax'
 
 
-def update_status(_current_playing):
-    current = spotify.current_user_playing_track()
-    print()
-    print(current)
-    if current == None:
-        muzon = "ᯤ Spotify isn't playing"
-    elif current['is_playing'] == False:
-        muzon = "ᯤ Spotify is paused"
-        
-    elif current["currently_playing_type"] == "track":
-        timestamp = timestp.give_min_sec_music(current['progress_ms'])
-        
-        track = current["item"]["name"]
-        #album = current["item"]["album"]["name"]
-        artist = current["item"]["artists"][0]["name"]
-        muzon = f"ᯤ Spotify | {timestamp} | {artist} - {track}"
-        
-    elif current["currently_playing_type"] == "episode":
-        timestamp = timestp.give_ho_mi_se_podcast(ms = current['progress_ms'])
-        muzon = f"ᯤ Spotify is playing a podcast | {timestamp}"
-        
+async def main():
+    client = await get_session()
 
-    if len(muzon) >= 53:                   
-        muzon = muzon[:50] + '...' + inst
-    else:
-        muzon += inst
-
-    with TelegramClient('host', os.getenv('TG_API_ID'), os.getenv('TG_API_HASH')) as client:
-        full = client(functions.users.GetFullUserRequest(user))
-        stat = full.full_user.about
-        if muzon != stat:
-            client(functions.account.UpdateProfileRequest(about=muzon))
-
-    return 
-
-
-
-
-if __name__ == '__main__':
     while True:
         try:
-            while True:
-                # print("Получаю обновления")
-                current_playing = (update_status(current_playing)) 
-                time.sleep(15)
+            current = spotify.current_user_playing_track()
+            # print(current)
+
+            if not current:
+                muzon = "ᯤ Spotify isn't playing"
+            elif not current['is_playing']:
+                muzon = "ᯤ Spotify is paused"
+            elif current["currently_playing_type"] == "track":
+                timestamp = timestp.give_min_sec_music(current['progress_ms'])
+                track = current["item"]["name"]
+                artist = current["item"]["artists"][0]["name"]
+                muzon = f"ᯤ Spotify | {timestamp} | {artist} - {track}"
+            elif current["currently_playing_type"] == "episode":
+                timestamp = timestp.give_ho_mi_se_podcast(ms=current['progress_ms'])
+                muzon = f"ᯤ Spotify is playing a podcast | {timestamp}"
+
+            if len(muzon) >= 53:
+                muzon = muzon[:50] + '...' + inst
+            else:
+                muzon += inst
+
+            full = await client(functions.users.GetFullUserRequest('me'))
+            stat = full.full_user.about
+
+            if muzon != stat:
+                await client(functions.account.UpdateProfileRequest(about=muzon))
+
+            await asyncio.sleep(15)
 
         except Exception as e:
             print(e)
-            time.sleep(30)
-input('Press ENTER to exit')
+            await asyncio.sleep(30)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- modules/auth.py
+++ modules/auth.py
@@ -1,34 +1,44 @@
+import qrcode
 import asyncio
+import aiofiles.os
 import os
 from telethon import TelegramClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-async def main():
+async def check_exists_session_telegram():
+    if await aiofiles.os.path.exists("host.session"):
+        print("success")
+        return True
+    else:
+        print("false") 
+        return False
+
+
+
+async def get_session():
     client = TelegramClient(
         'host',
         int(os.getenv('TG_API_ID')),
         os.getenv('TG_API_HASH')
     )
+    if not await check_exists_session_telegram():
+        try:
+            await client.connect()
+            qr = await client.qr_login()
+            img = qrcode.QRCode(border=1)
+            img.add_data(qr.url)
+            img.print_ascii()
+            print("Открой Telegram → Настройки → Устройства → Подключить устройство")
+            print("QR-ссылка (открой в браузере, если не хочешь ASCII):")
+            print(qr.url)
+            await qr.wait()   # ждём подтверждение
+            print("Авторизация завершена, сессия сохранена")
+            # await client.disconnect()
+        except asyncio.TimeoutError:
+            print("Авторизация не удалась :(")
+    return await client.start()
 
-    await client.connect()
-
-    qr = await client.qr_login()
-
-    import qrcode
-
-    img = qrcode.QRCode(border=1)
-    img.add_data(qr.url)
-    img.print_ascii()
-
-    print("Открой Telegram → Настройки → Устройства → Подключить устройство")
-    print("QR-ссылка (открой в браузере, если не хочешь ASCII):")
-    print(qr.url)
-
-    await qr.wait()   # ждём подтверждение
-
-    print("Авторизация завершена, сессия сохранена")
-    await client.disconnect()
-
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(get_session())
